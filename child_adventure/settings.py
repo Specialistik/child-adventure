047c0b5fbfb54cbee2622733aaeeb9831d6ea647
Specialistik <test04@list.ru>
2016-11-16 10:20:12 +0000

    mysql params

--- child_adventure/settings.py
+++ child_adventure/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'child_adventure.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'child_adventure',
+        'USER': 'root',
+	'PASSWORD': '1f53601c',
+	'HOST': 'localhost'
+
     }
 }
 
